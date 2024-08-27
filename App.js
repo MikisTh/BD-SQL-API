6717c9afd8f7d325874c8013dc61e39d3f9eadcf
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-27 11:02:51 -0300

    Update App.js
    Add

--- App.js
+++ App.js
@@ -15,6 +15,53 @@ con.connect((err) => {
     console.log('Connection established!')
 })
 
+con.query('SELECT * FROM author', (err, rows) => {
+    if (err) throw err
+
+    console.log('Authors: ', rows, '\n\n')
+})
+
+con.query('SELECT * FROM book', (err, rows) => {
+    if (err) throw err
+
+    console.log('Books: ', rows, '\n\n')
+})
+con.query(
+    'SELECT b.id, b.title, a.name, a.location FROM book as b INNER JOIN author as a ON b.author = a.id', 
+    (err, rows) => {
+    if (err) throw err
+
+    rows.forEach(row => {
+        console.log(`${row.title} by ${row.name}, ${row.location}`)
+    });
+})
+
+const newBook = {title: 'Computer Networks', author: 2}
+
+con.query(
+    'INSERT INTO book SET ?', newBook, (err, res) => {
+    if (err) throw err
+
+    console.log(`New book added with ID: ${res.insertId}`)
+})
+
+con.query('UPDATE book SET title = ? WHERE ID = ?', 
+    ['Redes de Computadores', 6],
+    (err,ressult) => {
+        if(err) throw err;
+
+        console.log('Changed ', ressult)
+    }
+)
+
+con.query(
+    'DELETE FROM book WHERE id = ?', [6], (err, result) => {
+        if (err) throw err;
+
+        console.log(`Deleted ${result.affectedRows} row(s)`);
+    }
+);
+
 con.end((err) => {
     if(err) {
         console.log('Erro to finish connection...', err)
